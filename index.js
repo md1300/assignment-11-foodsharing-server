02f2e7544d4bc85a2d963f86eb85566db46f8958
md1300 <md.tawhid.khan1998@gmail.com>
2024-10-11 16:02:22 +0600

    delete data from foodsCollection

--- index.js
+++ index.js
@@ -82,6 +82,16 @@ app.get('/added-food',async(req,res)=>{
       res.send(result)
   })
 
+  // ----------delete my posted food data from foods collection ---------
+
+  app.delete('/delete/:id',async(req,res)=>{
+    const id =req.params.id;
+    const query={_id:new ObjectId(id)}
+    const result=await foodsCollection.deleteOne(query)
+    res.send(result)
+    
+  })
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
